5f6eeefb3893900a4cc494f7ddac315fdf922960
Kirill P <kirkplat@gmail.com>
2021-04-05 21:40:41 +0300

    first commit

---
+++ src/components/libr.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <ul>
+            <spisok
+            v-for="(lib, i) of librir"
+            v-bind:lib="lib"
+            v-on:remove-lib="removelib"
+            v-on:edit-lib="editLib"
+            v-on:savelib="savelib"
+            v-bind:index="i"
+            />
+        </ul>
+    </div>
+</template>
+
+<script >
+    import spisok from '@/components/liiiil'
+    export default {
+        props: ['librir'],
+        components: {
+            spisok
+        },
+        methods: {
+            removelib(id) {
+                this.$emit('remove-lib', id)
+            },
+            editLib(id) {
+                this.$emit('edit-lib', id)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/liiiil.vue
@@ -0,0 +1,59 @@
+<template>
+  <table id="right">
+  <colgroup>
+    <col span="5" style="background:Khaki"><!-- С помощью этой конструкции задаем цвет фона для первых двух столбцов таблицы-->
+  </colgroup>
+  <tr>
+    <th>№ п/п</th>
+    <th>Имя</th>
+    <th>Год</th>
+    <th>Название</th>
+    <th>Количество страниц</th>
+    <th></th>
+  </tr>
+  <tr>
+    <td>{{index + 1}}</td>
+    <td>{{lib.name}}</td>
+    <td>{{lib.year}}</td>
+    <td>{{lib.naz}}</td>
+    <td>{{lib.kol}}</td>
+    <td>
+      <button class="rm" 
+        v-on:click="$emit('remove-lib', lib.id)">&times;
+      </button>
+      <button  
+        v-on:click="$emit('edit-lib', lib.id)"> ✎
+      </button>
+    </td>
+  </tr>
+ 
+</table>
+</template>
+
+<script>
+export default {
+    props: {
+        lib: {
+            type:Object,
+            reqired: true
+        },
+        index: Number
+    }
+    
+}
+</script>
+
+<style scoped>
+#right {
+  float:left;
+  width: 10%;
+
+}
+@media handheld {
+ #right{
+  width: 5%;
+ } 
+  
+  
+}
+</style>
